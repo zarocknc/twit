b876955aea149d754d76809412e3d469e011e8d9
zarocknc <sebastian_nicolaschong@protonmail.com>
2023-01-20 11:59:56 -0500

    using components

--- components/Dataviews/users/table.vue
+++ components/Dataviews/users/table.vue
@@ -1,13 +1,37 @@
 <template>
-    <div>
+    <div v-for="(user, id) in holi?.data">
 
+        <p class="dataHeaderStyle" @click="changeView(id)">{{ user.name }} </p>
+        <div :data-charapter-id="id" class="bg-gray-200 p-6 hidden">
+            <p>{{ user.email }}</p>
+            <p>{{ user.id }}</p>
+            <InputsText>cambiar email</InputsText>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 
+const { data: holi } = await useFetch('/api/notes')
+
+
+function changeView(id: number) {
+    const userInfo = document.querySelectorAll('[data-charapter-id="' + id + '"]')[0]
+    if (userInfo.classList.contains('block')) {
+        userInfo.classList.remove('block')
+        userInfo.classList.add('hidden')
+    } else {
+        userInfo.classList.remove('hidden')
+        userInfo.classList.add('block')
+    }
+}
+
+
 </script>
 
-<style scoped>
 
+<style scoped>
+.dataHeaderStyle {
+    @apply bg-gray-800 text-white p-6 cursor-pointer 
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,6 @@
 <template>
     <div>
-        <div v-for="(user, id) in holi?.data">
-        
-            <p class="bg-gray-800 text-white p-6 cursor-pointer" @click="changeView(id)">{{ user.name }} </p>
-            <div :data-charapter-id="id" class="bg-gray-200 p-6 hidden">
-                <p>{{ user.email }}</p>
-                <p>{{ user.id }}</p>
-            </div>
-        </div>
+        <DataviewsUsersTable />
 
         <div>
             contador {{ count }}
@@ -22,20 +15,10 @@
 
 <script setup lang="ts">
 
-    const { data: holi } = await useFetch('/api/notes')
 
     const count = ref(0)
 
-    function changeView(id:number) {
-        const userInfo = document.querySelectorAll('[data-charapter-id="' +id+ '"]')[0]
-        if(userInfo.classList.contains('block')){
-            userInfo.classList.remove('block')
-            userInfo.classList.add('hidden')
-        } else {
-            userInfo.classList.remove('hidden')
-            userInfo.classList.add('block')
-        }
-    }
+
 </script>
 
 <style lang="scss" scoped>
